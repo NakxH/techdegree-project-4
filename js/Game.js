99e0e3caef1b8e254f39a557fe796c864030bbdf
Hakan <74737999+NakxH@users.noreply.github.com>
2021-10-07 16:54:10 +0100

    Added Game class and Phrase Class and began to start adding methods and properties

--- js/Game.js
+++ js/Game.js
@@ -2,3 +2,33 @@
  * Project 4 - OOP Game App
  * Game.js */
 
+class Game {
+  constructor(){
+    this.missed
+    this.phrases = this.createPhrases()
+    this.activePhrase
+  }
+  createPhrases() {
+    let phrases = [
+      {
+        phrase: 'starwars'
+      },
+      {
+        phrase: 'forest gump'
+      },
+      {
+        phrase: 'titanic'
+      },
+      {
+        phrase: 'green mile'
+      },
+      {
+        phrase: 'shawshank redemption'
+      }
+    ]
+    return phrases
+  }
+  getRandomPhrase(){
+    let randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+  }
+}
